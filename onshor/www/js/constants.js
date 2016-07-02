c2bfa20e606b9a9cda9528081648d7b783955709
Amr <amr.essam67@stud.fci-cu.edu.eg>
2016-07-02 03:52:31 +0200

    watch position after getting user

--- onshor/www/js/constants.js
+++ onshor/www/js/constants.js
@@ -1,4 +1,4 @@
 angular.module('starter')
-.constant('BASE_URL',"/api") // during development
-// .constant('BASE_URL',"https://onshor-staging.herokuapp.com") // when deploying
+// .constant('BASE_URL',"/api") // during development
+.constant('BASE_URL',"https://onshor.herokuapp.com/") // when deploying
 .constant('LOCATION_UPDATE_INTERVAL', 300000) // 5 minutes = 5 * 60 * 1000
